d4a708579447866f5976e70e4eb46f50569e9920
Kevin Klues <klueska@cs.berkeley.edu>
2014-06-06 19:53:24 -0700

    Start using the profalarms for pprof
    We now pass the pprof test!

--- src/pkg/runtime/signal_akaros.c
+++ src/pkg/runtime/signal_akaros.c
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build darwin dragonfly freebsd linux netbsd openbsd solaris
-
 #include "runtime.h"
 #include "defs_GOOS_GOARCH.h"
 #include "os_GOOS.h"
@@ -78,18 +76,13 @@ runtime·sigdisable(uint32 sig)
 void
 runtime·resetcpuprofiler(int32 hz)
 {
-	Itimerval it;
-
-	runtime·memclr((byte*)&it, sizeof it);
-	if(hz == 0) {
-		runtime·setitimer(ITIMER_PROF, &it, nil);
+	if (hz == 0) {
+		runtime·disable_profalarm();
 	} else {
-		it.it_interval.tv_sec = 0;
-		it.it_interval.tv_usec = 1000000 / hz;
-		it.it_value = it.it_interval;
-		runtime·setitimer(ITIMER_PROF, &it, nil);
+		runtime·enable_profalarm(1000000 / hz);
 	}
 	m->profilehz = hz;
+	return;
 }
 
 void
@@ -102,17 +95,6 @@ os·sigpipe(void)
 void
 runtime·crash(void)
 {
-#ifdef GOOS_darwin
-	// OS X core dumps are linear dumps of the mapped memory,
-	// from the first virtual byte to the last, with zeros in the gaps.
-	// Because of the way we arrange the address space on 64-bit systems,
-	// this means the OS X core file will be >128 GB and even on a zippy
-	// workstation can take OS X well over an hour to write (uninterruptible).
-	// Save users from making that mistake.
-	if(sizeof(void*) == 8)
-		return;
-#endif
-
 	runtime·unblocksignals();
 	runtime·setsig(SIGABRT, SIG_DFL, false);
 	runtime·raise(SIGABRT);
