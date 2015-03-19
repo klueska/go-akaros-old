ac6e087c812b1ef0a5b4b09e586f68c7fa0738bd
Kevin Klues <klueska@cs.berkeley.edu>
2015-03-18 20:29:47 -0700

    Migrate all development to fork of golang

--- src/pkg/syscall/asm_akaros.s
+++
@@ -1,3 +0,0 @@
-// For some reason, Go freaks out if this file doesn't exist.
-// We may need to add some things in here eventually, anyway.
-
--- src/pkg/syscall/mkall_akaros.sh
+++
@@ -1,6 +0,0 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-GOROOT="$DIR/../../.."
-SRCDIR="$GOROOT/src"
-export GOROOT
-$DIR/mkall.sh $@
-rm -rf _obj
